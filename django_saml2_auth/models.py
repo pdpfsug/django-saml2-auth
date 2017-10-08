d9cf7b9d3da3e17ae24db3c990da16ff509986d4
Radeox <dawid.weglarz95@gmail.com>
2017-10-08 04:04:17 +0200

    [multiple-idp] model IdentityProvider

--- django_saml2_auth/models.py
+++ django_saml2_auth/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class IdentityProvider(models.Model):
+    """
+    Module to incapsulate information from IdP
 
+    """
+
+    url_metadata = models.URLField(primary_key=True)
+    cert = models.TextField()
+    attributes_map = models.TextField(
+            help_text="JSON object to map IdP returned attributes to user model fields")  # Text for portability
+    is_enabled = models.BooleanField(
+            default=True,
+            help_text="Enable or disable this IdP")
